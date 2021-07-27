269a3ac3335afb29c915ed081c5c2c2f7db707a9
dd0sxx <contacttheotelonis@gmail.com>
2021-07-27 19:24:04 -0400

    reinstalled hardhat so tests would work because I was having some dependency issues

---
+++ test/EvenlyDistribute.js
@@ -0,0 +1,17 @@
+// const { expect } = require("chai");
+
+// describe("EvenlyDistribute contract", function () {
+//   it("Deployment should work and assign owner", async function () {
+//     const [owner] = await ethers.getSigners();
+    
+//     const EvenlyDistribute = await ethers.getContractFactory("EvenlyDistribute");
+
+//     console.log('meow')
+//     // console.log('owner: ', owner)
+
+//     const evenlyDistribute = await EvenlyDistribute.deploy();
+
+//     const contractOwner = await evenlyDistribute.owner();
+//     expect(owner).to.equal(contractOwner);
+//   });
+// });
