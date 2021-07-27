8cadb6d403b64bf4dfdf051ce45cc61ce6114330
dd0sxx <contacttheotelonis@gmail.com>
2021-07-27 12:17:33 -0400

    added external contracts Ownable and SafeMath to inherit from

---
+++ contracts/EvenlyDistribute.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract EvenlyDistribute is Ownable {
+
+    using SafeMath for uint;
+
+
+}
